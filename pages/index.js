9db70cae6614059bd069055b095573efb00fa452
Patty13132 <gpbmanalang.student@ua.edu.ph>
2023-08-25 01:00:10 +0800

    update

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,17 @@ export default function Home() {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>Hello I'am a 3rd year Computer Engineering Student</p>
+        <h2>My Impression of the first month as a CPE 3rd year student.</h2>
+        <p>
+          I'm expecting to improve myself in this school year. 
+          I know this is gonna be challenging year for me than last year.
+          I'm looking forward too to our professors to teach us very well.
+          I'm expecting to have a good schedule this school year.
+          I'm expecting to face more challenging activities.
+        </p>
+        <p>
+          I know now How to Update my Website.
+        </p>
         <p>
           (This is a sample website - you’ll be building a site like this on{' '}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
